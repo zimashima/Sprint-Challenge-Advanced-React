ace7b4904a4f9aa46e123755af7262da0a634c5f
Mashima <52222646+zimashima@users.noreply.github.com>
2019-12-06 16:48:23 -0500

    added useLocalStorage custom hook

--- client/src/components/useLocalStorage.js
+++ client/src/components/useLocalStorage.js
@@ -1,1 +1,20 @@
-import React from 'react'
+import {useState} from 'react'
+
+export const useLocalStorage = (key, initialValue) => {
+
+    window.localStorage.setItem(key, initialValue)
+
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = window.localStorage.getItem(key);
+        console.log(item)
+        return item ? JSON.parse(item) : initialValue;
+      });
+
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value))
+    };
+
+    return [storedValue, setValue]
+
+};
